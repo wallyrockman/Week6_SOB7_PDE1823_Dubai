9ad9692fa04a7cbd95da864fe8e1901ef0d27e4a
wally <147479633+wallyrockman@users.noreply.github.com>
2026-03-03 14:37:09 +0400

    Refactor Cowbull game logic and update for Python 3
    Refactored compare_numbers function to count cows and bulls accurately. Updated input method for Python 3 compatibility and ensured random number is always 4 digits.

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -2,12 +2,23 @@ import random
 
 def compare_numbers(number, user_guess):
     ## your code here
+    cowbull = [0, 0] # [cows, bulls]
+    # Ensure both are treated as 4-character strings for comparison
+    number = number.zfill(4)
+    user_guess = user_guess.zfill(4)
+    
+    for i in range(len(number)):
+        if user_guess[i] == number[i]:
+            cowbull[1] += 1
+        elif user_guess[i] in number:
+            cowbull[0] += 1
     return cowbull
 
 playing = True #gotta play the game
-number = str(random.randint(0,9999)) #random 4 digit number
+# Added .zfill(4) to ensure a number like 42 becomes "0042"
+number = str(random.randint(0,9999)).zfill(4) #random 4 digit number
 guesses = 0
-print number
+# print number # We do not need this line of code.
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +27,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess! ") # raw_input is used in Python 2.x, but in Python 3.x, we should use input() instead.
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
