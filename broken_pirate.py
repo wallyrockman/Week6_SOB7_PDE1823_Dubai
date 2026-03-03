aa2965fd850d566ce8f214c1e1ca625da853a49a
wally <147479633+wallyrockman@users.noreply.github.com>
2026-03-03 14:12:04 +0400

    Fix syntax errors in broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
+greeting = input("Hello, possible pirate! What's the password? ") # Missing colon at the end of this line
 if greeting in ["Arrr!"]: # NG replaced ) by ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # We do not use elif here, we can just use 'else'. We can also remove the rest of the condition
+	print("Greetings, hater of pirates!")
